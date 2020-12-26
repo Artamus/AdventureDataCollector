b6f748393312a9323e3f2d0cb9d0107ba371f1d5
Artamus <markusloide@gmail.com>
2020-12-27 01:17:48 +0200

    using events and the ingame API instead of hooking into frames

--- AdventureDataCollector.lua
+++ AdventureDataCollector.lua
@@ -1,6 +1,6 @@
-local db
 local defaultDb = {missionData = {}}
 local _, addonTbl = ...
+local b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" -- You will need this for encoding/decoding
 local hooksecurefunc = _G["hooksecurefunc"]
 
 AdventureDataCollector = CreateFrame("Frame", "AdventureDataCollector", UIParent)
@@ -8,7 +8,7 @@ AdventureDataCollector.initDone = false
 
 SLASH_ADVENTUREDATACOLLECTOR1 = "/adc"
 
-function SerializeValue(val, type)
+local function SerializeValue(val, type)
     if type == "string" then
         return '"' .. val .. '"'
     end
@@ -24,7 +24,7 @@ function SerializeValue(val, type)
     return val
 end
 
-function SerializeJson(obj)
+local function SerializeJson(obj)
     local type = type(obj)
 
     if type ~= "table" then
@@ -57,85 +57,142 @@ function SerializeJson(obj)
     return json
 end
 
-function AdventureDataCollector:SetupTabsHook(...)
-    hooksecurefunc(_G["CovenantMissionFrame"], "InitiateMissionCompletion", AdventureDataCollector.MissionCompleteHook)
+-- TODO: Might be another way to find this part
+local function GetCompletedMissionInfo(missionID)
+    local completedMissions = C_Garrison.GetCompleteMissions(123) -- Finds complete covenant missions if the argument is 123, I don't know why this works
+    for i = 1, #completedMissions do
+        if completedMissions[i].missionID == missionID then
+            return completedMissions[i]
+        end
+    end
 end
 
-function AdventureDataCollector:MissionCompleteHook(...)
-    local missionFrame = _G["CovenantMissionFrame"]
-    local missionComplete = missionFrame.MissionComplete
-    local mission = missionComplete.currentMission
+local function GetAutoCombatSpells(rawSpells)
+    if not rawSpells then
+        return nil
+    end
+
+    local spells = rawSpells
+    for _, spell in pairs(spells) do
+        spell.previewMask, spell.schoolMask, spell.icon, spell.spellTutorialFlag = nil
+    end
+
+    return spells
+end
+
+local function EncodeBase64(data)
+    return ((data:gsub(
+        ".",
+        function(x)
+            local r, b = "", x:byte()
+            for i = 8, 1, -1 do
+                r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and "1" or "0")
+            end
+            return r
+        end
+    ) .. "0000"):gsub(
+        "%d%d%d?%d?%d?%d?",
+        function(x)
+            if (#x < 6) then
+                return ""
+            end
+            local c = 0
+            for i = 1, 6 do
+                c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
+            end
+            return b:sub(c + 1, c + 1)
+        end
+    ) .. ({"", "==", "="})[#data % 3 + 1])
+end
+
+function AdventureDataCollector:GARRISON_MISSION_COMPLETE_RESPONSE(
+    event,
+    missionID,
+    _canComplete,
+    _success,
+    _bonusRollSuccess,
+    _followerDeaths,
+    autoCombatResult)
+
+    -- I do not care about the combat log
+    local missionName = C_Garrison.GetMissionName(missionID)
+    local mission = GetCompletedMissionInfo(missionID)
 
     local missionDetails = {
         dataVersion = 1,
         missionName = mission.name,
-        winner = missionComplete.autoCombatResult.winner,
+        winner = autoCombatResult.winner,
         missionLevel = mission.missionScalar,
-        isRare = mission.encounterIconInfo.isRare,
-        isElite = mission.encounterIconInfo.isElite,
+        isRare = mission.isRare, -- TODO: Figure out if elite means anything and if I need it
         level = mission.level,
         isMaxLevel = mission.isMaxLevel
     }
 
-    if mission.environmentEffect then
-        table.insert(missionDetails, mission.environmentEffect.name)
-        table.insert(missionDetails, mission.environmentEffect.autoCombatSpellInfo)
+    -- Environment effects
+    local environmentEffect = C_Garrison.GetAutoMissionEnvironmentEffect(missionID)
+    if environmentEffect then
+        if environmentEffect.autoCombatSpellInfo then
+            local spellInfo = environmentEffect.autoCombatSpellInfo
+            spellInfo.previewMask, spellInfo.schoolMask, spellInfo.icon, spellInfo.spellTutorialFlag = nil
+        end
+
+        missionDetails.environmentEffect = environmentEffect
     end
 
-    -- Loop over enemies
+    -- Enemies
+    local encounters = C_Garrison.GetMissionCompleteEncounters(missionID)
     local enemies = {}
-    for k, v in pairs(missionComplete.missionEncounters) do
+    for _, v in pairs(encounters) do
+        local autoCombatSpells = GetAutoCombatSpells(v.autoCombatSpells)
+
         local enemy = {
             boardIndex = v.boardIndex,
             attackPower = v.attack,
+            health = v.health,
             maxHealth = v.maxHealth,
             role = v.role,
             name = v.name,
-            spells = v.autoCombatSpells
+            autoCombatSpells = autoCombatSpells
         }
-
         table.insert(enemies, enemy)
     end
     missionDetails.enemies = enemies
 
-    -- Loop over followers
+    -- Followers
     local followers = {}
-    for k, v in pairs(missionComplete.followerGUIDToInfo) do
-        local follower = {
-            boardIndex = v.boardIndex,
-            attackPower = v.autoCombatantStats.attack,
-            maxHealth = v.autoCombatantStats.maxHealth,
-            currentHealth = v.autoCombatantStats.currentHealth,
-            level = v.level,
-            name = v.name,
-            quality = v.quality,
-            role = v.role,
-            className = v.className
-        }
+    for _, followerID in pairs(mission.followers) do
+        local followerInfo = C_Garrison.GetFollowerMissionCompleteInfo(followerID)
+        local followerCombatStats = C_Garrison.GetFollowerAutoCombatStats(followerID)
 
-        table.insert(followers, follower)
+        local followerCombatSpells = C_Garrison.GetFollowerAutoCombatSpells(followerID, followerInfo.level)
+        local autoCombatSpells = GetAutoCombatSpells(followerCombatSpells)
+
+        local follower = {
+            boardIndex = followerInfo.boardIndex,
+            attackPower = followerCombatStats.attack,
+            maxHealth = followerCombatStats.maxHealth,
+            currentHealth = followerCombatStats.currentHealth,
+            level = followerInfo.level,
+            name = followerInfo.name,
+            quality = followerInfo.quality,
+            role = followerInfo.role,
+            className = followerInfo.className,
+            spells = autoCombatSpells
+        }
+        followers[followerID] = follower
     end
     missionDetails.followers = followers
 
-    table.insert(db.missionData, missionDetails)
+    AdventureDataCollectorDB = AdventureDataCollectorDB or defaultDb
+    table.insert(AdventureDataCollectorDB.missionData, missionDetails)
 end
 
-function AdventureDataCollector:ADDON_LOADED(event, addon)
-    if addon == "Blizzard_GarrisonUI" then
-        AdventureDataCollector:Init()
+function AdventureDataCollector:EventHandler(event, ...)
+    if self[event] then
+        self[event](self, event, ...)
+    else
+        print("No handler for event " .. event)
     end
-end
-
-function AdventureDataCollector:Init()
-    if self.initDone then
-        return
-    end
-    hooksecurefunc(_G["CovenantMissionFrame"], "SetupTabs", self.SetupTabsHook)
-
-    AdventureDataCollectorDB = AdventureDataCollectorDB or {missionData = {}}
-    db = AdventureDataCollectorDB
-
-    self.initDone = true
 end
 
 SlashCmdList.ADVENTUREDATACOLLECTOR = function(msg, editBox)
@@ -146,13 +203,13 @@ SlashCmdList.ADVENTUREDATACOLLECTOR = function(msg, editBox)
     end
 
     if msg == "clear" then
-        db = defaultDb
+        AdventureDataCollectorDB = defaultDb
     elseif msg == "dump" then
-        local jsonSerialized = SerializeJson(db)
-        local frame = addonTbl.GetExportFrame(jsonSerialized)
+        local jsonSerialized = SerializeJson(AdventureDataCollectorDB.missionData)
+        local frame = addonTbl.GetExportFrame(EncodeBase64(jsonSerialized))
         frame:Show()
     end
 end
 
-AdventureDataCollector:RegisterEvent("ADDON_LOADED")
-AdventureDataCollector:SetScript("OnEvent", AdventureDataCollector.ADDON_LOADED)
+AdventureDataCollector:RegisterEvent("GARRISON_MISSION_COMPLETE_RESPONSE")
+AdventureDataCollector:SetScript("OnEvent", AdventureDataCollector.EventHandler)
